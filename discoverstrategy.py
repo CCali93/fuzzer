ddbaacb3b09a99724ddbb4aa4e0e02a42b423e14
Curtis Cali <botkilla2k12@gmail.com>
2014-09-15 22:54:16 -0400

    Added first code for discovery algorithm

---
+++ discoverstrategy.py
@@ -0,0 +1,6 @@
+class DiscoverStrategy:
+    def __init__(self, args):
+        self.argsDictionary = self.parseArgs(args)
+
+    def parseArgs(self, args):
+        return {}
--- fuzzer.py
+++ fuzzer.py
@@ -1,8 +1,17 @@
 import requests
+import sys
+
+from discoverstrategy import DiscoverStrategy
 
 #stub function as a placeholder for future code
 def main(args):
-    r = requests.get('https://api.github.com/events')
+    command = arg[0]
+    strategy = None
+
+    if command == "discover":
+        strategy = DiscoverStrategy(args[1:])
+    else:
+        print("USAGE fuzz [discover | test] url OPTIONS")
 
 if __name__ == '__main__':
-    main([])
+    main(sys.argv[1:])
