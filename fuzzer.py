8bf428a7c7b02c8e365404a851bcc3f067b4fc36
Curtis Cali <botkilla2k12@gmail.com>
2014-09-15 20:48:24 -0400

    Added a new main file
    Contains stub code.

---
+++ fuzzer.py
@@ -0,0 +1,8 @@
+import requests
+
+#stub function as a placeholder for future code
+def main(args):
+    r = requests.get('https://api.github.com/events')
+
+if __name__ == '__main__':
+    main([])
